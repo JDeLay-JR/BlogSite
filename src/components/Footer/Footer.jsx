3b3637e88435c627d4c6cc12d82125cec7750c83
JDeLay-JR <jdelay.jr@gmail.com>
2018-11-06 22:55:09 -0500

    feat: more fixes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,19 +3,6 @@ import "./Footer.css";
 
 class Footer extends Component {
   render() {
-    const PoweredBy = props => {
-      const { show } = props;
-      if (show) {
-        return (
-          <section className="poweredby">
-            Proudly published with <a href="https://gatsbyjs.org">Gatsby</a>
-          </section>
-        );
-      }
-      return null;
-    };
-
-    const { promoteGatsby } = this.props;
     const { label, url, year } = this.props.copyright;
     return (
       <footer className="site-footer clearfix">
@@ -23,7 +10,6 @@ class Footer extends Component {
           <a href={url || "/"}>{label}</a> &copy;{" "}
           {year || new Date().getFullYear()}
         </section>
-        <PoweredBy show={promoteGatsby} />
       </footer>
     );
   }
